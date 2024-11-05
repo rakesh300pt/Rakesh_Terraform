f9409bf6c724bf17765f21a7d39e33cdcb3a0c56
rakesh <rakesh@gmail.com>
2024-11-05 17:38:08 +0000

    azure with terrform variable file

--- variable.tf
+++ variable.tf
@@ -1,33 +1,72 @@
-
-variable "varstring" {
-  type = string
+variable "rg_name" {
+  default = "Rakesh_terraform_rg"
 }
 
-variable "varnumber" {
-  type = number
+variable "location" {
+  default = "centralindia"
 }
 
-variable "varboolean" {
-  type = bool
+variable "address_space" {
+  default = ["10.1.0.0/16"]
+  type    = list(string)
+}
+variable "vnet_name" {
+  default = "R_terraform_VM"
 }
 
-variable "varlist" {
-  type = list(string)
+variable "subnet_name" {
+  default = "R_terraform_snet"
 }
 
-variable "varmap" {
-  type = map(string)
+variable "subnet_space" {
+  default = ["10.1.0.0/24", "10.1.10.0/24"]
+  type    = list(string)
 }
 
-variable "vartuple" {
-  type = tuple([string, number])
+variable "key" {
+  default = "ssh_key"
 }
-variable "varobject" {
-  type = object({
-    name = string
-    age  = number
-  })
+
+variable "nic_name" {
+  default = "nic2"
+}
+variable "nic3_name" {
+  default = "nic3"
+}
+
+variable "ip_name" {
+  default = "ip"
+}
+variable "pip_name" {
+  default = "pip"
+}
+variable "pip1_name" {
+  default = "pip1"
 }
 
 
+variable "vm_name" {
+  default = "LinuxVM"
+
+}
+variable "winvm_name" {
+  default = "WindowsVM"
+
+}
+
+variable "admin" {
+  default = "azureuser"
+
+}
+
+variable "size" {
+  default = "Standard_B1s"
+}
+variable "user" {
+  default = "azureuser"
+}
+
+variable "nsg" {
+  default = "nsg"
+}
 
