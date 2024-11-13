aecf608f379d5604976a56aeb9ad9939ca2587b1
Ubuntu <azureuser@r-linux.qrljlpexxywefcunlkmgl2looc.rx.internal.cloudapp.net>
2024-11-13 09:59:18 +0000

    azure dns resource files

---
+++ azure-dns/variable.tf
@@ -0,0 +1,29 @@
+variable "rg_name" {
+  default = "r_rg"
+}
+
+variable "location" {
+  default = "centralindia"
+}
+
+
+variable "test1_name" {
+  default = "dnsvm1"
+}
+
+variable "test2_name" {
+  default = "dnsvm2"
+}
+
+variable "vnet1_name" {
+  default = "vnet1"
+}
+
+variable "vnet2_name" {
+  default = "vnet2"
+}
+
+variable "network_name" {
+  default = "mynet"
+}
+
